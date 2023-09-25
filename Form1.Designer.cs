6e80f4f54e34410023ab782edc5290c41ac4ce39
JakubHabelski <93724769+JakubHabelski@users.noreply.github.com>
2023-09-25 21:40:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Fi_kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.circuit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.iso = new System.Windows.Forms.Label();
+            this.iso_size = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(41, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Obwód:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // circuit
+            // 
+            this.circuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.circuit.Location = new System.Drawing.Point(219, 88);
+            this.circuit.Name = "circuit";
+            this.circuit.Size = new System.Drawing.Size(137, 38);
+            this.circuit.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(41, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FI po obiekcie:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(47, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "oblicz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // iso
+            // 
+            this.iso.AutoSize = true;
+            this.iso.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.iso.Location = new System.Drawing.Point(41, 153);
+            this.iso.Name = "iso";
+            this.iso.Size = new System.Drawing.Size(167, 31);
+            this.iso.TabIndex = 5;
+            this.iso.Text = "Grubość iso:";
+            this.iso.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // iso_size
+            // 
+            this.iso_size.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.iso_size.Location = new System.Drawing.Point(219, 153);
+            this.iso_size.Name = "iso_size";
+            this.iso_size.Size = new System.Drawing.Size(137, 38);
+            this.iso_size.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(41, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fi z izolacją:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(290, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 31);
+            this.label4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(290, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 31);
+            this.label5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(390, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "W [cm]";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(390, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 31);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "W [mm]";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(146, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(230, 31);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Kalkulator średnic";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(390, 316);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 31);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "W [cm]";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(390, 364);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 31);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "W [cm]";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 481);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.iso_size);
+            this.Controls.Add(this.iso);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.circuit);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox circuit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label iso;
+        private System.Windows.Forms.TextBox iso_size;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fi_kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Height = 200;
+            label3.Width = 200; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(iso_size.Text) && string.IsNullOrEmpty(circuit.Text))
+            {
+                MessageBox.Show("wpisz Fi i grubość iso!!!");
+                return;
+            }
+            if (string.IsNullOrEmpty(circuit.Text))
+            {
+                MessageBox.Show("wpisz Fi!!!");
+                return;
+            }
+            if (string.IsNullOrEmpty(iso_size.Text))
+            {
+                MessageBox.Show("wpisz grubość iso!!!");
+                return;
+            }
+            
+            int cir = int.Parse(circuit.Text);            
+            int iso = int.Parse(iso_size.Text);
+            
+            double diam = cir / 3.14;  // fi bez izolacji
+            diam = Math.Round(diam, 1);
+            double diam_iso = (cir / 3.14) + (iso/10 * 2);  // fi z izolacją
+            diam_iso = Math.Round(diam_iso, 1);
+            label4.Text = diam.ToString();
+            label5.Text = diam_iso.ToString();
+            }
+        }
+    }
+
